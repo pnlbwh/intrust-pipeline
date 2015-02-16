47e82261b80f07b5a7c08e1d114107a09cf02881
reckbo <reckbo@bwh.harvard.edu>
2015-02-16 18:08:22 -0500

    +Add SetUpData config files

---
+++ SetUpData.sh
@@ -0,0 +1,43 @@
+source SetUpData_inputs.sh
+
+# Atlas mask
+# Inputs
+atlas_target=$t1align
+atlas_trainingstructs=trainingt1s.txt
+atlas_traininglabels=trainingmasks.txt
+# Output
+t1atlasmask=$case/strct/$case.t1atlasmask.nrrd
+
+# Freesurfer
+# Inputs
+fs_t1=$t1align
+fs_mask=$t1atlasmask
+# Output
+fs=$case/strct/$case.freesurfer
+
+# DWI preprocessing
+# Inputs:
+dwipipeline_dwi=$dwiraw
+# Output
+dwi_ed=$case/diff/$case.dwi-Ed.nrrd  
+
+# DWI mask
+# Inputs:
+dwibetmask_dwi=$dwi_ed
+# Output
+dwibetmask=$case/diff/$case.dwibetmask.nrrd  
+
+# UKF
+# Inputs
+ukf_dwi=$dwi_ed
+ukf_dwimask=$dwibetmask
+# Output
+ukf=$case/diff/$case.ukf_2T.vtk.gz
+
+# Freesurfer to DWI registration
+# Inputs
+fs2dwi_dwi=$dwi_ed
+fs2dwi_dwimask=$dwibetmask
+fs2dwi_fssubjectdir=$fs
+# Output
+fsindwi=$case/diff/$case.wmparc_in_dwi.nrrd
