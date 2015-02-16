39b68fb58c0f2ffcde2bba6c47d0f8a81ebb8c12
reckbo <reckbo@bwh.harvard.edu>
2015-02-16 18:21:52 -0500

    +Add setup.sh

---
+++ setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -eu
+
+# Set this, then run `./setup.sh`
+pnlutil=/projects/pnl/software/pnlutil/
+
+
+dofiles="\
+default.dwi-Ed.nrrd.do         \
+default.dwibetmask.nrrd.do     \
+default.freesurfer.do          \
+default.ukf_2T.vtk.gz.do       \
+default.wmparc_in_dwi.nrrd.do  \
+"
+
+for dofile in $dofiles; do
+    [ -e "$dofile" ] || cp $pnlutil/$dofile .
+done
+
+[ -e default.t1atlasmask.nrrd.do ] || cp $pnlutil/default.atlaslabelmap.nrrd.do default.t1atlasmask.nrrd.do   
